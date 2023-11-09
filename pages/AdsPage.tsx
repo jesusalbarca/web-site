10ac39435a502e63bdf5de74c342ea43c6acfa87
jesusalbarca <100383415@alumnos.uc3m.es>
2023-11-09 01:33:43 +0100

    fix try ads

--- pages/AdsPage.tsx
+++ pages/AdsPage.tsx
@@ -1,35 +1,39 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 /* eslint-disable jsx-a11y/accessible-emoji */
-import React from 'react'
-import { NextPage } from 'next'
-import Head from 'next/head'
-import { motion } from 'framer-motion'
-import { stagger, fadeInUp } from '@src/containers/framerAnimations'
-import Icons from '../src/components/Icons/Icons'
+import React from 'react';
+import { GetServerSideProps, NextPage } from 'next';
+import Head from 'next/head';
 
-const AdsPage: NextPage = ({ isTxtRequest }: { isTxtRequest?: boolean }) => {
-    // Si se está solicitando el archivo ads.txt, devuelve el contenido necesario
-    if (isTxtRequest) {
-      return (
-        <div>
-          google.com, pub-7217160576021116, DIRECT, f08c47fec0942fa0
-        </div>
-      );
-    }
-  
-    // Si no es una solicitud de ads.txt, puedes renderizar otro contenido o componente
-    return (
-      <>
-        {/* Aquí se puede colocar el contenido o la estructura de la página para la ruta /ads */}
-      </>
-    );
+const AdsPage: NextPage = () => {
+  return (
+    <div>
+      {/* Aquí puedes definir la estructura de tu página de anuncios */}
+      <p>Contenido de la página de anuncios.</p>
+    </div>
+  );
+};
+
+export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
+  // Obtener la ruta solicitada
+  const { url } = req;
+
+  // Verificar si la solicitud es para /ads.txt
+  if (url === '/ads.txt') {
+    // Establecer el contenido para /ads.txt
+    res.setHeader('Content-Type', 'text/plain');
+    res.write('google.com, pub-7217160576021116, DIRECT, f08c47fec0942fa0');
+    res.end();
+
+    // Devolver un objeto vacío ya que no hay datos para cargar en la página /ads
+    return {
+      props: {},
+    };
+  }
+
+  // Si la solicitud no es para /ads.txt, se carga la página normal
+  return {
+    props: {},
   };
-  
-  AdsPage.getInitialProps = ({ req }: any) => {
-    // Verificar si la solicitud es para /ads.txt
-    const isTxtRequest = req && req.url === '/ads.txt';
-  
-    return { isTxtRequest };
-  };
-  
-  export default AdsPage;
+};
+
+export default AdsPage;
